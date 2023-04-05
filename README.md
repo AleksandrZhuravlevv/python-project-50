4bbb65061e8b4846099d2d0b919de6b8ba9e170b
Aleksandr Zhuravlev <Alexandrzhuravlyov828@yandex.ru>
2023-04-05 12:09:46 +0700

    step/шаг 6

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/AleksandrZhuravlevv/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/AleksandrZhuravlevv/python-project-50/actions)
 
 <a href="https://asciinema.org/a/J5cwK0YzH31a6OQqstrsCOB8x" target="_blank"><img src="https://asciinema.org/a/J5cwK0YzH31a6OQqstrsCOB8x.svg" /></a>
+
+<a href="https://asciinema.org/a/TFc11utzkT5tJEvgRRR2pdbuY" target="_blank"><img src="https://asciinema.org/a/TFc11utzkT5tJEvgRRR2pdbuY.svg" /></a>
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -12,9 +12,9 @@ def main():
     )
     parser.add_argument("first_file", type=str)
     parser.add_argument("second_file", type=str)
-    parser.add_argument("-f", "--format", type=str, help="set format of output") 
+    parser.add_argument("-f", "--format", type=str, default='json', help="set format of output") 
     args = parser.parse_args()
-    diff = generate_diff(args.first_file, args.second_file)
+    diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
     
 if __name__ == "__main__":
