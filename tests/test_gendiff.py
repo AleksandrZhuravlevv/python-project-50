7664682dcc61bb19c69516563aceb3242764d4d5
Aleksandr Zhuravlev <Alexandrzhuravlyov828@yandex.ru>
2023-04-06 11:40:52 +0700

    доработка перед проверкой

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -11,7 +11,7 @@ def test_generate_stylish():
     diff_yml_stylish = generate_diff(
         "tests/fixtures/file1.yml", "tests/fixtures/file2.yml", "stylish"
     )
-    result_true_stylish = open("tests/fixtures/result_true_stylish").read()
+    result_true_stylish = open("tests/fixtures/true_stylish").read()
 
     assert diff_json_stylish == result_true_stylish
     assert diff_yaml_stylish == result_true_stylish
@@ -28,7 +28,7 @@ def test_generate_plain():
     diff_yml_plain = generate_diff(
         "tests/fixtures/file1.yml", "tests/fixtures/file2.yml", "plain"
     )
-    result_true_plain = open("tests/fixtures/result_true_plain").read()
+    result_true_plain = open("tests/fixtures/true_plain").read()
 
     assert diff_json_plain == result_true_plain
     assert diff_yaml_plain == result_true_plain
@@ -45,7 +45,7 @@ def test_generate_json():
     diff_yml_json = generate_diff(
         "tests/fixtures/file1.yml", "tests/fixtures/file2.yml", "json"
     )
-    result_true_json = open("tests/fixtures/result_true_json.json").read()
+    result_true_json = open("tests/fixtures/true_json").read()
 
     assert diff_json_json == result_true_json
     assert diff_yaml_json == result_true_json
