63cd316ba1b5d40ce37ee0d7a81d0f55c91e5a3a
Aleksandr Zhuravlev <Alexandrzhuravlyov828@yandex.ru>
2023-04-10 19:06:04 +0700

    Исправление ошибок

--- gendiff/plain.py
+++ gendiff/plain.py
@@ -1,27 +1,38 @@
 def plain(diff_dict):
     return make_sub_string(diff_dict['children'], "")
 
+
 def make_sub_string(sub_list, path_name):
     sub_string = ""
     for item_dict in sub_list:
         if item_dict['type'] == 'added':
-            sub_string += make_added_string(item_dict['key'], item_dict['value'], path_name)
+            sub_string += make_added_string(
+                item_dict['key'], item_dict['value'], path_name)
         elif item_dict['type'] == 'deleted':
             sub_string += make_deleted_string(item_dict['key'], path_name)
         elif item_dict['type'] == 'updated':
-            sub_string += make_updated_string(item_dict['key'], item_dict['value1'], item_dict['value2'], path_name)
+            sub_string += make_updated_string(
+                item_dict['key'], item_dict['value1'],
+                item_dict['value2'], path_name)
         elif 'children' in item_dict:
-            sub_string += make_sub_string(item_dict['children'], path_name + item_dict['key'] + ".")
+            sub_string += make_sub_string(
+                item_dict['children'], path_name + item_dict['key'] + ".")
     return sub_string
 
+
 def make_added_string(key, value, path_name):
-    return f"Property '{path_name}{key}' was added with value: {map_value(value)}\n"
+    return f"Property '{path_name}{key}' " \
+           f"was added with value: {map_value(value)}\n"
+
 
 def make_deleted_string(key, path_name):
     return f"Property '{path_name}{key}' was removed\n"
 
+
 def make_updated_string(key, value1, value2, path_name):
-    return f"Property '{path_name}{key}' was updated. From {map_value(value1)} to {map_value(value2)}\n"
+    return f"Property '{path_name}{key}'" \
+           f" was updated. From {map_value(value1)} to {map_value(value2)}\n"
+
 
 def map_value(key_value):
     if isinstance(key_value, dict):
--- gendiff/stylish.py
+++ gendiff/stylish.py
@@ -1,44 +1,35 @@
-def build_diff(tree, depth=0):
-    result = ""
-    indent = build_indent(depth)
-
-    if tree["type"] == "root":
-        result += "{\n"
-        for child in tree["children"]:
-            result += build_diff(child, depth + 1)
-        result += f"{indent}}}"
-    else:
-        name_of_property = tree["key"]
-        type_of_property = tree["type"]
-        value = map_value(tree["value"], depth)
-
-        if type_of_property == "parent":
-            result += f"{indent}{name_of_property}: {{\n"
-            for child in tree["children"]:
-                result += build_diff(child, depth + 1)
-            result += f"{indent}}}\n"
-
-        elif type_of_property == "added":
-            result += f"{indent}+ {name_of_property}: {value}\n" # noqa
-
-        elif type_of_property == "deleted":
-            result += f"{indent}- {name_of_property}: {value}\n" # noqa
-
-        elif type_of_property == "unchanged":
-            result += f"{indent}  {name_of_property}: {value}\n" # noqa
-
-        elif type_of_property == "updated":
-            old_value = map_value(tree["value1"], depth)
-            new_value = map_value(tree["value2"], depth)
-            result += f"{indent}- {name_of_property}: {old_value}\n" # noqa
-            result += f"{indent}+ {name_of_property}: {new_value}\n" # noqa
-
-    return result
+def stylish(diff_dict):
+    sub_string = make_sub_string(diff_dict["children"], "", 1)
+    return "{\n" + sub_string + "}"
 
 
-def map_value(key_value, depth):
+def make_sub_string(sub_list, sub_string, depth):  # noqa: C901
+    for item_dict in sub_list:
+        if "children" in item_dict:
+            sub_string += " " * (4 * depth) + item_dict["key"] + ": {\n"
+            sub_string = make_sub_string(
+                item_dict["children"], sub_string, depth + 1)
+            sub_string += " " * (4 * depth) + "}\n"
+        elif item_dict["type"] == "added":
+            sub_string += " " * (4 * depth) + "+ " + item_dict["key"] + ": "
+            sub_string += map_value(item_dict["value"]) + "\n"
+        elif item_dict["type"] == "deleted":
+            sub_string += " " * (4 * depth) + "- " + item_dict["key"] + ": "
+            sub_string += map_value(item_dict["value"]) + "\n"
+        elif item_dict["type"] == "unchanged":
+            sub_string += " " * (4 * depth) + item_dict["key"] + ": "
+            sub_string += map_value(item_dict["value"]) + "\n"
+        elif item_dict["type"] == "updated":
+            sub_string += " " * (4 * depth) + "- " + item_dict["key"] + ": "
+            sub_string += map_value(item_dict["value1"]) + "\n"
+            sub_string += " " * (4 * depth) + "+ " + item_dict["key"] + ": "
+            sub_string += map_value(item_dict["value2"]) + "\n"
+    return sub_string
+
+
+def map_value(key_value):
     if isinstance(key_value, dict):
-        return map_dict_value(key_value, depth + 1)
+        return "{\n" + make_sub_string(key_value.items(), "", 1) + " " * 4 + "}"
     elif key_value is True:
         return "true"
     elif key_value is False:
@@ -47,22 +38,3 @@ def map_value(key_value, depth):
         return "null"
     else:
         return str(key_value)
-
-
-def map_dict_value(value, depth):
-    indent = build_indent(depth)
-    sub_string = ""
-    for key, val in value.items():
-        if isinstance(val, dict):
-            sub_string += f"{indent}    {key}: {{\n"
-            sub_string += f"{map_dict_value(val, depth + 1)}"
-            sub_string += f"{indent}    }}\n"
-        else:
-            val = map_value(val, depth)
-            sub_string += f"{indent}    {key}: {val}\n"
-
-    return sub_string
-
-
-def build_indent(depth):
-    return "    " * depth
